a9b8072f92ef5d36db882b99a25af8693d2a258e
sarawone <sarawone.poudal@gmail.com>
2026-06-19 13:47:50 +0100

    done exericse

--- foundations/05_joinStrings/joinStrings-example.js
+++ foundations/05_joinStrings/joinStrings-example.js
@@ -1,9 +1,14 @@
-const birthYear = 1947;
-const thisYear = 1965;
-const firstName = "Carlos";
+const firstName ="Carlos";
 const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+
+
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 console.log(greeting);
 
